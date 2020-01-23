02b6e48a1023f972131fb890a280c5c83c9a2f46
Nicolas_Cadet <c.nicolas334@gmail.com>
2020-01-23 15:26:27 +0100

    Commit with a lot of writings in html's

--- Spacer/Spacer/settings.py
+++ Spacer/Spacer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4'
+    'bootstrap4',
     'accounts',
 ]
 
@@ -122,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
