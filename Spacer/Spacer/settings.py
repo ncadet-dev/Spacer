ea3af3f79256d3613c39b758be65396e91c8ef1e
Nicolas CADET <c.nicolas334@gmail.com>
2020-01-26 22:27:58 +0100

    Nearly finished

--- Spacer/Spacer/settings.py
+++ Spacer/Spacer/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+print(TEMPLATES_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [STATIC_DIR]
+print(STATICFILES_DIRS)
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
